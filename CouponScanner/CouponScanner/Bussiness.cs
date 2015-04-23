3fe30b40bf478b8f7e4ea8b48ddd559822d98c43
sagi baz <sharpen6@gmail.com>
2015-04-23 13:06:30 +0300

    Bussiness class created
    Attributes only

--- CouponScanner/CouponScanner/Bussiness.cs
+++ CouponScanner/CouponScanner/Bussiness.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CouponScanner
+{
+    class Bussiness
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Location {get;set;}
+        public List<Coupon> Sells { get; set; }
+        public int OwnedBy { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
