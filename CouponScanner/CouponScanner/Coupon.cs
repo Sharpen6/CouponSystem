d4106f05bcdaaced8f63fe3197d1ebf3d51fcbb5
sagi baz <sharpen6@gmail.com>
2015-04-23 12:58:59 +0300

    Coupon class created
    Attributes only

---
+++ CouponScanner/CouponScanner/Coupon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouponScanner
+{
+    class Coupon
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double OriginalPrice { get; set; }
+        public double DiscountPrice { get; set; }
+        public DateTime ExperationDate { get; set; }
+        public double AvarageRanking { get; set; }
+    }
+}
